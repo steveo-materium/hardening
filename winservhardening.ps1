633e0746ee8bc8050e8a73638554627ec341a1e3
steveo-materium <54216179+steveo-materium@users.noreply.github.com>
2024-10-14 10:13:16 +0100

    Create winservhardening.ps1

---
+++ winservhardening.ps1
@@ -0,0 +1,30 @@
+### Hardening and pre-reqs script
+##
+###
+
+Function InstallHardeningKitty() {
+    $Version = (((Invoke-WebRequest "https://api.github.com/repos/scipag/HardeningKitty/releases/latest" -UseBasicParsing) | ConvertFrom-Json).Name).SubString(2)
+    $HardeningKittyLatestVersionDownloadLink = ((Invoke-WebRequest "https://api.github.com/repos/scipag/HardeningKitty/releases/latest" -UseBasicParsing) | ConvertFrom-Json).zipball_url
+    $ProgressPreference = 'SilentlyContinue'
+    New-Item -ItemType Directory -Force -Path C:\bin
+    Set-Location -Path C:\bin
+    Invoke-WebRequest $HardeningKittyLatestVersionDownloadLink -Out HardeningKitty$Version.zip
+    Expand-Archive -Path ".\HardeningKitty$Version.zip" -Destination ".\HardeningKitty$Version" -Force
+    $Folder = Get-ChildItem .\HardeningKitty$Version | Select-Object Name -ExpandProperty Name
+    Move-Item ".\HardeningKitty$Version\$Folder\*" ".\HardeningKitty$Version\"
+    Remove-Item ".\HardeningKitty$Version\$Folder\"
+    New-Item -Path $Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version -ItemType Directory
+    Set-Location .\HardeningKitty$Version
+    Copy-Item -Path .\HardeningKitty.psd1,.\HardeningKitty.psm1,.\lists\ -Destination $Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version\ -Recurse
+    Import-Module "$Env:ProgramFiles\WindowsPowerShell\Modules\HardeningKitty\$Version\HardeningKitty.psm1"
+}
+InstallHardeningKitty
+
+### Backing up initial config state prior to hardening
+Invoke-HardeningKitty -Mode Config -Report -ReportFile C:\bin\$env:COMPUTERNAME-$(get-date -format ddmmyy)-initialstate-hardening_report.csv
+## Running CIS Windows Server User Settings
+Invoke-HardeningKitty -Mode HailMary -FileFindingList ./lists/finding_list_cis_microsoft_windows_server_2019_1809_2.0.0_user.csv -SkipRestorePoint
+## Running CIS Windows Server Computer Settings
+Invoke-HardeningKitty -Mode HailMary -FileFindingList ./lists/finding_list_cis_microsoft_windows_server_2019_1809_2.0.0_computer.csv -SkipRestorePoint
+
+restart-computer
